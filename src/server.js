bf34369c95a3c349ce75b3f7a0ed738a75e45133
william <williamborba97@gmail.com>
2022-12-31 02:14:36 -0300

    novas rotas

--- src/server.js
+++ src/server.js
@@ -1,7 +1,32 @@
+//call express
+const { response } = require('express');
 const express = require('express');
 
+//execute express
 const app = express();
 
-const PORT = 3333
+//create a route and show a message
+app.get('/message/:id/:name',(request,response)=>{
+const {id , name} = request.params;
 
-app.listen(PORT,() =>console.log(`server is running on port ${PORT}`));
+response.send(`
+ID da mensagem ${id}
+nome do usuario${name}`)
+});
+
+app.get('/users',(request,response)=>{
+const {page,limit} = request.params;
+
+response.send(`
+pagina: ${page}
+limite: ${limit} `)
+})
+
+
+//create a PORT
+const PORT = 3333;
+
+//show a message in the console of where PORT is running
+app.listen(PORT,()=>console.log(`Server is running on port${PORT}`));
+
+//users?page=10&limit=4

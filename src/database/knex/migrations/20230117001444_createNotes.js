a417da7acd3433577ef707b20d776933b197e1d1
william <williamborba97@gmail.com>
2023-01-19 18:52:28 -0300

    migration com query builder knex

--- src/database/knex/migrations/20230117001444_createNotes.js
+++ src/database/knex/migrations/20230117001444_createNotes.js
@@ -2,6 +2,7 @@
 exports.up = knex => knex.schema.createTable("notes",table=> {
 table.increments("id");
 table.text("title");
+table.text("descrition");
 table.integer("user_id").references("id").inTable("users");
 
 table.timestamp("created_at").default(knex.fn.now())
@@ -9,4 +10,4 @@ table.timestamp("updated_at").default(knex.fn.now())
 });
 
 
-exports.down =  knex => knex.schema.dropTable(table);
+exports.down =  knex => knex.schema.dropTable("notes");

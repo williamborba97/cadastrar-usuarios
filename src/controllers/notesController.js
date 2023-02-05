1776fe640b9c6ee3f2a4ecddd4678c6a73b8adea
william <williamborba97@gmail.com>
2023-02-05 18:42:42 -0300

    new filters of search add

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -61,12 +61,22 @@ class NotesController{
     }
 
     async index(request,response){
-        const {user_id} =request.query;
-        
-        const notes = await knex("notes")
-        .where({user_id})
-        .orderBy("title");
+        const {tags,title,user_id} =request.query;
 
+        let notes;
+
+        if(tags){
+            const filterTags = tags.split(",").map(tag =>tag.trim());
+
+            notes = await knex("tags")
+            .whereIn("name",filterTags)
+            
+        } else {
+            notes = await knex("notes")
+                .where({user_id})
+                .whereLike("title",`%${title}%`)
+                .orderBy("title");
+        }
         return response.json(notes);
     }
 
